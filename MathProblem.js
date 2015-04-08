864d6d55264335e8d0b1996d5f6594d94d95dc45
surfusa <surfusa@users.noreply.github.com>
2015-04-07 19:14:35 -0700

    Create MathProblem.js

---
+++ MathProblem.js
@@ -0,0 +1,13 @@
+function Addition() 
+{
+    var a = Math.floor(Math.random() * 10) + 1;
+    var b = Math.floor(Math.random() * 10) + 1;
+    var op = ["*", "+", "/", "-"][Math.floor(Math.random()*4)];
+    return prompt("How much is " + a + " " + op + " " + b + "?") == eval( a + op + b);
+}
+
+var questions = [Addition() , Addition() ],
+    total = questions.length,
+    correct = questions.filter(Boolean).length;
+
+alert( "You got "+correct+"/"+total+" correctly");
