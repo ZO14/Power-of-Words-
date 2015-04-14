eb111674c678dd39659b1185f12b31abc1a100f9
surfusa <surfusa@users.noreply.github.com>
2015-04-13 18:53:49 -0700

    Update MathProblem.js

--- MathProblem.js
+++ MathProblem.js
@@ -1,13 +1,21 @@
-function Addition() 
+function G2Subtraction() //Grade 2 subtraction problem
 {
-    var a = Math.floor(Math.random() * 10) + 1;
-    var b = Math.floor(Math.random() * 10) + 1;
-    var op = ["*", "+", "/", "-"][Math.floor(Math.random()*4)];
-    return prompt("How much is " + a + " " + op + " " + b + "?") == eval( a + op + b);
+    var x, y, input;
+    x = Math.floor(Math.random() * 20 + 10)
+    y = Math.floor(Math.random() * 10 + 5) 
+    write("Lyla has two family members with the same birthday. So she makes two cakes "
+    + "and tops them with a total of "+ x + " candles. One cake has " + y + " candles. "
+    + "How many candles are on the other cake?")
+   
+    input = document.getElementById("numb").value;
+
+    if (input != x - y)
+    {
+        write("That is incorrect. Would you like some help?");
+    }
+    else
+    {
+        write("That is correct!")
+    }
+    return ;
 }
-
-var questions = [Addition() , Addition() ],
-    total = questions.length,
-    correct = questions.filter(Boolean).length;
-
-alert( "You got "+correct+"/"+total+" correctly");
